d2b6402879a070b91c6061c172338f8165692a96
zbf1999 <zw949146236@outlook.com>
2020-06-11 22:03:13 +0800

    2020.6.11

--- 小项目_exercise/remove_background.py
+++ 小项目_exercise/remove_background.py
@@ -4,7 +4,7 @@
 from removebg import RemoveBg
 rmbg = RemoveBg("your key", "error.log")     #fill in your key
 path = 'image/removebg'
-rmbg.remove_background_from_img_file(f"D:/Desktop/1.jpg")    #照片path
+rmbg.remove_background_from_img_file(f"D:/Desktop/9.jpg")    #照片path
 
 #解决pip请求超时
 #pip install -i https://pypi.tuna.tsinghua.edu.cn/simple some-package
